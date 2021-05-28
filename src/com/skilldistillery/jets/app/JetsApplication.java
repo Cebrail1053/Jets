05bc84f43cf62c0ea58b22c73de4e29af17c1a7f
Gabriel Avila <gabriel.avila.ga09@gmail.com>
2021-05-28 15:23:21 -0600

    initial commit

---
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -0,0 +1,19 @@
+package com.skilldistillery.jets.app;
+
+import java.util.Scanner;
+
+import com.skilldistillery.jets.entity.AirField;
+
+public class JetsApplication {
+	private AirField airField;
+	static Scanner kb = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		JetsApplication app = new JetsApplication();
+		app.launch();
+	}
+
+	public void launch() {
+
+	}
+}
